5152fe5fc9db12141f5e119464f9e649d56c9fe0
Brendan Krekeler <krekeler@brendans-mbp.lan>
2018-06-15 00:33:38 -0500

    Completed the Variables and Constants

--- Swift Basics/Swift Basics/Swift Basics/main.swift
+++ Swift Basics/Swift Basics/Swift Basics/main.swift
@@ -6,5 +6,16 @@
 //  Copyright © 2018 Brendan Krekeler. All rights reserved.
 //
 
-
-
+let sample1: UInt8 = 0x3A
+var sample2: UInt8 = 58
+var heartRate: Int = 85
+var deposit: Double = 135002796
+let accelceration: Float = 9.800
+var mass: Float = 14.6
+var distance: Double = 129.763001
+var lost: Bool = true
+var expensive: Bool = true
+var choice: Int = 2
+let integral: Character = "\u{222b}"
+let greeting: String = "Hello"
+var name: String = "Karen"
